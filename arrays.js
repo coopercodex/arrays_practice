58d4d809b76b3e0eeafe5934c39f150c008c258b
Derek Cooper <derekcooper20@gmail.com>
2022-06-08 14:19:37 -0600

    Array method jobs

--- arrays.js
+++ arrays.js
@@ -2,3 +2,15 @@
 var names = ['kitty', 'holly', 'ebay', 'sally'];
 var nums = [9, 4, 9, 10];
 var words [true, false, true, true];
+
+// this method adds 'derek' to the end of the array//
+names.push('derek');
+ // this will create and add 'derek' to index 4
+
+// this method removes the first element from the array//
+nums.shift();
+// this will remove 9 at index 0 which equals the first element
+
+// this method adds an element to the beginning of the array//
+words.unshift(false);
+// this will add false to index 0 which is now false and move true over one 
